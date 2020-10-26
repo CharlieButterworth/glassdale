24fea9160fe284b692c144d19c92919d0f0486ea
Charles Butterworth <charlessbutterworth@gmail.com>
2020-10-26 13:09:21 -0500

    got the alibilist.js in and placed the butotn

--- scripts/criminals/Criminals.js
+++ scripts/criminals/Criminals.js
@@ -1,11 +1,32 @@
+const eventHub = document.querySelector(".container")
+
 export const Criminal = (criminalObj) => {
     return `
-    <div class="criminalsContainer">
+    <div id="criminal-${criminalObj.id}"class="criminalsContainer">
     <p class="name">Name: ${criminalObj.name}</p>  
      <p class="age">Age: ${criminalObj.age}</p>
      <p class="Conviction">Conviction: ${criminalObj.conviction}</p>
      <p class="term start">Start Date: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p>
      <p class="term end">End Date: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p>
+     <button id="associates--${criminalObj.id}">Associate Alibis</button>
      </div>
      `
 }
+
+eventHub.addEventListener("click", (eventObj) => {
+    const [nameOfId, criminalId] = eventObj.target.id.split("--")
+
+    if(eventObj.target.id.startsWith("associates--")) {
+
+        const myCustomEvent = new CustomEvent("alibiButtonClicked", {
+            detail: {
+                criminalId: criminalId
+            }
+        })
+
+
+        eventHub.dispatchEvent(myCustomEvent)
+    }
+
+
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { NoteForm } from "../notes/NoteForm.js";
 import { NoteList } from "../notes/NoteList.js";
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
+import { createAlibiEventListener } from "./criminals/AlibiList.js";
 import { CriminalList } from "./criminals/CriminalList.js";
 
 import { OfficerList } from "./officers/OfficerList.js";
@@ -13,3 +14,5 @@ ConvictionSelect()
 OfficerSelect()
 NoteForm()
 NoteList()
+
+createAlibiEventListener()
